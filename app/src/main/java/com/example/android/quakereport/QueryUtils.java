a4920e75a7cf9fb92b72648fc05d380e9c0fc9e8
Ankur Charan <ankurcharan98@gmail.com>
2020-06-24 15:43:44 +0530

    timestamp to string
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -7,7 +7,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Helper methods related to requesting and receiving earthquake data from USGS.
@@ -61,9 +63,15 @@ public final class QueryUtils {
 
                 String magnitude = properties.getString("mag");
                 String place = properties.getString("place");
-                String date = properties.getString("time");
 
-                earthquakes.add(new Earthquake(magnitude, place, date));
+                // from timestamp to human-readable format
+                String time = properties.getString("time");
+                Date date = new Date(Long.parseLong(time));     // date takes long in constructor
+
+                SimpleDateFormat dateFormatter = new SimpleDateFormat("MMM DD, yyyy");
+                String dateToDisplay = dateFormatter.format(date);
+
+                earthquakes.add(new Earthquake(magnitude, place, dateToDisplay));
             }
 
         } catch (JSONException e) {
