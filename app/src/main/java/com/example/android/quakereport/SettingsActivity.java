652489f193b936659c9aa0d4472769d8a384d28c
Ankur Charan <ankurcharan98@gmail.com>
2020-06-25 13:59:49 +0530

    adding min-max magnitude setting
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -10,10 +10,15 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_acitivity);
+        setContentView(R.layout.settings_activity);
     }
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment {
 
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
